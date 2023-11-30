edb83822cd44e5026dd949179c26ab7d97015dcc
AnonymousBit <68566858+AnonymousBit0111@users.noreply.github.com>
2023-11-30 12:30:40 +0300

    simplified entry point

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,8 @@
-#include <SDL2/SDL.h>
-#include <SDL_events.h>
-#include <SDL_render.h>
-#include <SDL_video.h>
-#include <iostream>
 
 #include "Core/Application.h"
-#include "Core/Event.h"
-#include "Core/EventHandlers.h"
-#include "Core/Window.h"
-
 
 int main() {
   TerraMorph::Core::Application app;
-  app.Init();
-  app.Run();
+  app.init();
+  app.run();
 }
