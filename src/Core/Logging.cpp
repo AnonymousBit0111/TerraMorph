7e0446e42eb84e4dd8253645be74acf592aa7213
AnonymousBit <68566858+AnonymousBit0111@users.noreply.github.com>
2023-12-01 09:17:48 +0300

    improved format of messages

--- src/Core/Logging.cpp
+++ src/Core/Logging.cpp
@@ -4,14 +4,14 @@
 using namespace TerraMorph::Core;
 
 void Logging::warn(const std::string &message) {
-  std::cout << "WARNING:" << message + "\n";
+  std::cout << "[WARNING] " << message + "\n";
 }
 
 void Logging::error(const std::string &message) {
-  std::cerr << "ERROR" << message + "\n";
+  std::cerr << "[ERROR] " << message + "\n";
   exit(-1);
 }
 
 void Logging::log(const std::string &message) {
-  std::cout << "INFORMATION:" << message + "\n";
+  std::cout << "[INFORMATION] " << message + "\n";
 }
